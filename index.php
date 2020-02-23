48ea5c52ce803548c8999b5371d9a8be615e85d5
Đỗ Hồng Sơn <sondhph09397@fpt.edu.vn>
2020-02-23 16:31:53 +0700

    v5

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="./public/css/main.css">
-    <title>Bán vé xe khách - vvv Trang chủ</title>
+    <title>Bán vé xe khách - Trang chủ</title>
 </head>
 
 <body>
