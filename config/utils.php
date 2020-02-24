83cdbeebf9a5c148dff17a235f485fde590c812d
helgrind-cyber <helgrindxxx@gmail.com>
2020-02-24 19:00:20 +0700

    edit 24-2
    thầy giáo sửa

--- config/utils.php
+++ config/utils.php
@@ -22,4 +22,39 @@ function getdbConn() {
     }
 }
 
+function queryExecute($sql, $fetchAll = false){
+
+    $conn = getdbConn();
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    if($fetchAll){
+        $data = $stmt->fetchAll();
+    }else{
+        $data = $stmt->fetch();
+    }
+    return $data;
+}
+
+# kiểm tra xem user đã đăng nhập hay chưa
+function checkAdminLoggedIn(){
+    // kiểm tra đăng nhập
+    // 1 - đăng nhập thành công - ktra bằng session AUTH
+    if(!isset($_SESSION[AUTH]) || $_SESSION[AUTH] == null || count($_SESSION[AUTH]) == 0){
+        header('location: ' . BASE_URL . 'login.php?msg=Hãy đăng nhập');
+        die;
+    }
+    // 2 - giá trị của cột role_id = 2
+    if($_SESSION[AUTH]['role_id'] < 2){
+        header('location: ' . BASE_URL . 'login.php?msg=Bạn không có quyền truy cập');
+        die;
+    }
+}
+
+function dd($data){
+    echo "<pre>";
+	var_dump($data);
+	die;
+}
+
+
 ?>
