ee7b59f3106b0ca44a3de37558637c3f31a4b000
thietchu9910 <thietcvph09326@fpt.edu.vn>
2020-03-07 21:58:41 +0700

    gia diẹn
    giao diện
    
    Co-Authored-By: Đỗ Hồng Sơn <sondhph9397@users.noreply.github.com>

--- search/index.php
+++ search/index.php
@@ -32,43 +32,58 @@ $allData = queryExecute($getAllDataQuery, true);
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+   
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- bootstrap 4 -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!-- font awesome -->
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <!-- date picker -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css" />
-    <!-- main.css -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.0-alpha14/css/tempusdominus-bootstrap-4.min.css" />
+    <title>Trang chủ</title>
+    <!-- Favicon ico -->
+    <link rel="shortcut icon" type="image/png" href="<?= PUBLIC_URL . 'images/favicon.ico' ?>" />
+    <!-- Bootstrap -->
+    <link href="<?= THEME_ASSET_URL ?>dist/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="<?= THEME_ASSET_URL ?>assets/css/custom.css" rel="stylesheet" media="screen">
+    <!-- Carousel -->
+    <link href="<?= THEME_ASSET_URL ?>examples/carousel/carousel.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="<?= THEME_ASSET_URL ?>assets/js/html5shiv.js"></script>
+    <script src="<?= THEME_ASSET_URL ?>assets/js/respond.min.js"></script>
+<![endif]-->
 
-    <link rel="stylesheet" href="<?php echo PUBLIC_URL . '/css/main.css' ?>">
+    <!-- Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:700,400,300,300italic' rel='stylesheet' type='text/css'>
+    <!-- Font-Awesome -->
+    <link rel="stylesheet" type="text/css" href="<?= THEME_ASSET_URL ?>assets/css/font-awesome.css" media="screen" />
+    <!--[if lt IE 7]><link rel="stylesheet" type="text/css" href="<?= THEME_ASSET_URL ?>assets/css/font-awesome-ie7.css" media="screen" /><![endif]-->
+
+    <!-- REVOLUTION BANNER CSS SETTINGS -->
+    <link rel="stylesheet" type="text/css" href="<?= THEME_ASSET_URL ?>css/fullscreen.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="<?= THEME_ASSET_URL ?>rs-plugin/css/settings.css" media="screen" />
+
+    <!-- Picker UI-->
+    <link rel="stylesheet" href="<?= THEME_ASSET_URL ?>assets/css/jquery-ui.css" />
+
+    <!-- jQuery -->
+    <script src="<?= THEME_ASSET_URL ?>assets/js/jquery.v2.0.3.js"></script>
+
+
     <title>Bán vé xe khách - Trang chủ</title>
 </head>
 
 <body>
-    <header class="border-bottom">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-2 dashboard-logo">
-                    <a href="<?php BASE_URL ?>">
-                        <img src="<?php echo PUBLIC_URL . 'images/logo1.ico' ?>" alt="LOGO" class="">
-                    </a>
-                </div>
-                <div class="col-md-10">
-                    <ul class="navbar navbar-expand-md dashboard-nav nav justify-content-end">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="<?php echo BASE_URL ?>">Trang Chủ</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Thông tin</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?php echo SEARCH_URL?>">Danh sách vé</a>
-                        </li>
-                        <li class="nav-item" style="<?php if ($loggedInUser !== null && $loggedInUser['role_id'] > 1) {
+     
+     <div class="navbar-wrapper2">
+            <div class="container">
+            <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#"><img src="<?= THEME_ASSET_URL ?>images/logo1.ico" width="100px" alt="" /></a>
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+     
+      <li><a class="nav-link" href="#">Thông tin</a></li>
+      <li><a class="nav-link" href="<?php echo SEARCH_URL?>">Danh sách vé</a></li>
+      <li class="nav-item" style="<?php if ($loggedInUser !== null && $loggedInUser['role_id'] > 1) {
                                                         echo 'display:inline-block';
                                                     }else {echo 'display:none';} ?>">
                             <a class="nav-link text-danger font-weight-bold text-uppercase" href="<?php echo ADMIN_URL . 'dashboard' ?>">Quản lý Website</a>
@@ -92,12 +107,17 @@ $allData = queryExecute($getAllDataQuery, true);
                         <li class="btn-cart nav-item btn btn-outline-primary">
                             <a class="nav-link" href="<?php echo CART_URL ?>" title="">Giỏ Hàng <i class="fa fa-shopping-basket" aria-hidden="true"></i></a>
                         </li>
-                    </ul>
-                    <!-- </div> -->
-                </div>
+    </ul>
+  </div>
+</nav>
+                
+                
+
             </div>
         </div>
-    </header>
+    </div>
+
+   
     <!--END HEADER - START LIST-->
     <div class="container">
         <div class="row">
@@ -133,81 +153,158 @@ $allData = queryExecute($getAllDataQuery, true);
         </div>
     </div>
     <!--END LSIT - START NEWS-->
-    <div class="contact pt-4 pb-2 mt-5 border-top border-bottom">
-        <div class="container">
-            <div class="row">
-                <div class="col-3">
-                    <ul class="contact-list">
-                        <h4 class="h6 text-capitalize font-weight-bold">bến xe</h4>
-                        <li><a href="#" class="text-capitalize">mỹ đình</a></li>
-                        <li><a href="#" class="text-capitalize">mỹ đình</a></li>
-                        <li><a href="#" class="text-capitalize">mỹ đình</a></li>
-                        <li><a href="#" class="text-capitalize">mỹ đình</a></li>
-                    </ul>
+    <div class="new pt-2 pb-5">
+    <div class="container">
+                    <h1>Thông tin</h1>
+                     <div class="wrapper">
+                    <div class="list_carousel">
+                        <ul id="foo">
+                            <li>
+                                <a href="list4.html"><img src="<?= THEME_ASSET_URL ?>images/thumb-hawaii.jpg" alt=""/></a>
+                                <div class="m1">
+                                    <h6 class="lh1 dark"><b>Thông Tin</b></h6>
+                                    <h6 class="lh1 green">Save up to 30%</h6>
+                                </div>
+                            </li>
+                            <li>
+                                <a href="list4.html"><img src="<?= THEME_ASSET_URL ?>images/thumb-santorini.jpg" alt=""/></a>
+                                <div class="m1">
+                                    <h6 class="lh1 dark"><b>Thông Tin</b></h6>
+                                    <h6 class="lh1 green">Save up to 30%</h6>
+                                </div>
+                            </li>
+                            <li>
+                                <a href="list4.html"><img src="<?= THEME_ASSET_URL ?>images/thumb-dubai.jpg" alt=""/></a>
+                                <div class="m1">
+                                    <h6 class="lh1 dark"><b>Thông Tin</b></h6>
+                                    <h6 class="lh1 green">Save up to 30%</h6>
+                                </div>
+                            </li>
+                            <li>
+                                <a href="list4.html"><img src="<?= THEME_ASSET_URL ?>images/thumb-hawaii.jpg" alt=""/></a>
+                                <div class="m1">
+                                    <h6 class="lh1 dark"><b>Thông Tin</b></h6>
+                                    <h6 class="lh1 green">Save up to 30%</h6>
+                                </div>
+                            </li>
+                            <li>
+                                <a href="list4.html"><img src="<?= THEME_ASSET_URL ?>images/thumb-santorini.jpg" alt=""/></a>
+                                <div class="m1">
+                                    <h6 class="lh1 dark"><b>Thông Tin</b></h6>
+                                    <h6 class="lh1 green">Save up to 30%</h6>
+                                </div>
+                            </li>
+                            <li>
+                                <a href="list4.html"><img src="<?= THEME_ASSET_URL ?>images/thumb-dubai.jpg" alt=""/></a>
+                                <div class="m1">
+                                    <h6 class="lh1 dark"><b>Thông Tin</b></h6>
+                                    <h6 class="lh1 green">Save up to 30%</h6>
+                                </div>
+                            </li>
+                        </ul>
+                        <div class="clearfix"></div>
+                        <a id="prev_btn" class="prev" href="#"><img src="<?= THEME_ASSET_URL ?>images/spacer.png" alt=""/></a>
+                        <a id="next_btn" class="next" href="#"><img src="<?= THEME_ASSET_URL ?>images/spacer.png" alt=""/></a>
+                    </div>
                 </div>
-                <div class="col-3">
-                    <ul class="contact-list">
-                        <h4 class="h6 text-capitalize font-weight-bold">nhà xe</h4>
-                        <li><a href="#" class="text-capitalize">mỹ đình</a></li>
-                        <li><a href="#" class="text-capitalize">mỹ đình</a></li>
-                        <li><a href="#" class="text-capitalize">mỹ đình</a></li>
-                        <li><a href="#" class="text-capitalize">mỹ đình</a></li>
-                    </ul>
                 </div>
-                <div class="col-3">
-                    <ul class="contact-list">
-                        <h4 class="h6 text-capitalize font-weight-bold">tuyến đường</h4>
-                        <li><a href="#" class="text-capitalize">mỹ đình</a></li>
-                        <li><a href="#" class="text-capitalize">mỹ đình</a></li>
-                        <li><a href="#" class="text-capitalize">mỹ đình</a></li>
-                        <li><a href="#" class="text-capitalize">mỹ đình</a></li>
-                    </ul>
-                </div>
-                <div class="col-3">
-                    <ul class="contact-list">
-                        <h4 class="h6 text-capitalize font-weight-bold">mạng xã hội</h4>
-                        <li>
-                            <a href="#" class="text-capitalize">
-                                <i class="fa fa-facebook-square" aria-hidden="true"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="text-capitalize">
-                                <i class="fa fa-instagram" aria-hidden="true"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="text-capitalize">
-                                <i class="fa fa-instagram" aria-hidden="true"></i>
-                            </a>
-                        </li>
-                    </ul>
+       
+    <!--END CONTACT - START FOOTER-->
+    <div class="footerbg">
+            <div class="container">
 
+                <div class="col-md-3">
+                    <span class="ftitle">Mạng xã hội</span>
+                    <div class="scont">
+                        <a href="#" class="social1b"><img src="<?= THEME_ASSET_URL ?>images/icon-facebook.png" alt="" /></a>
+                        <a href="#" class="social2b"><img src="<?= THEME_ASSET_URL ?>images/icon-twitter.png" alt="" /></a>
+                        <a href="#" class="social3b"><img src="<?= THEME_ASSET_URL ?>images/icon-gplus.png" alt="" /></a>
+                        <a href="#" class="social4b"><img src="<?= THEME_ASSET_URL ?>images/icon-youtube.png" alt="" /></a>
+                        <br /><br /><br />
+                        <img src="<?= THEME_ASSET_URL ?>images/logo1.ico" width="100px" alt="" /><br />
+                        &copy; 2013 | <a href="#">Privacy Policy</a><br />
+                        All Rights Reserved
+                        <br /><br />
+
+                    </div>
                 </div>
+                <!-- End of column 1-->
+
+                <div class="col-md-3">
+                    <span class="ftitle">Điểm đi</span>
+                    <br /><br />
+                    <ul class="footerlist">
+                        <li><a href="#">Hà Nội</a></li>
+                        <li><a href="#">Hải Phòng</a></li>
+                        <li><a href="#">Nam Định</a></li>
+                        <li><a href="#">Bắc Ninh</a></li>
+                        <li><a href="#">Hà Nam</a></li>
+                        <li><a href="#">Ninh Bình</a></li>
+                    </ul>
+                </div>
+                <!-- End of column 2-->
+
+                <div class="col-md-3">
+                    <span class="ftitle">Điểm đến</span>
+                    <br /><br />
+                    <ul class="footerlist">
+                        <li><a href="#">Hà Nội</a></li>
+                        <li><a href="#">Hải Phòng</a></li>
+                        <li><a href="#">Nam Định</a></li>
+                        <li><a href="#">Bắc Ninh</a></li>
+                        <li><a href="#">Hà Nam</a></li>
+                        <li><a href="#">Ninh Bình</a></li>
+                    </ul>
+                </div>
+                <!-- End of column 3-->
+
+                <div class="col-md-3 grey">
+                    <span class="ftitle">Liên lạc</span>
+                    <div class="relative">
+                        <input type="email" class="form-control fccustom2" id="exampleInputEmail1" placeholder="Nhập email">
+                        <button type="submit" class="btn btn-default btncustom">Submit<img src="<?= THEME_ASSET_URL ?>images/arrow.png" alt="" /></button>
+                    </div>
+                    <br /><br />
+                    <span class="ftitle">Hỗ trợ</span><br />
+                    <span class="pnr">024.5333.344</span><br />
+                    office@travel.com
+                </div>
+                <!-- End of column 4-->
+
             </div>
         </div>
     </div>
-    <!--END CONTACT - START FOOTER-->
-    <footer>
-        <div class="container pt-2 text-center">
-            <p>Công ty Company</p>
-            <p>Địa chỉ: Lầu 8,9, Tòa nhà CirCO, 222 Điện Biên Phủ, Quận 3, TP. Hồ Chí Minh, Việt Nam</p>
-        </div>
-    </footer>
 
     <!-- jquery -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-    <!-- popper -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <!-- boottrap.js -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <!-- moment.js -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-    <!-- bootstrap datepicker -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
-    <!-- <script type="text/javascript" src="./ru.js"> -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.0-alpha14/js/tempusdominus-bootstrap-4.min.js"></script>
-    </script>
+    <script src="<?= THEME_ASSET_URL ?>assets/js/js-index3.js"></script>
+
+<!-- Custom functions -->
+<script src="<?= THEME_ASSET_URL ?>assets/js/functions.js"></script>
+
+<!-- Picker UI-->
+<script src="<?= THEME_ASSET_URL ?>assets/js/jquery-ui.js"></script>
+
+<!-- Easing -->
+<script src="<?= THEME_ASSET_URL ?>assets/js/jquery.easing.js"></script>
+
+<!-- jQuery KenBurn Slider  -->
+<script type="text/javascript" src="<?= THEME_ASSET_URL ?>rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+
+<!-- Nicescroll  -->
+<script src="<?= THEME_ASSET_URL ?>assets/js/jquery.nicescroll.min.js"></script>
+
+<!-- CarouFredSel -->
+<script src="<?= THEME_ASSET_URL ?>assets/js/jquery.carouFredSel-6.2.1-packed.js"></script>
+<script src="<?= THEME_ASSET_URL ?>assets/js/helper-plugins/jquery.touchSwipe.min.js"></script>
+<script type="text/javascript" src="<?= THEME_ASSET_URL ?>assets/js/helper-plugins/jquery.mousewheel.min.js"></script>
+<script type="text/javascript" src="<?= THEME_ASSET_URL ?>assets/js/helper-plugins/jquery.transit.min.js"></script>
+<script type="text/javascript" src="<?= THEME_ASSET_URL ?>assets/js/helper-plugins/jquery.ba-throttle-debounce.min.js"></script>
+
+<!-- Custom Select -->
+<script type='text/javascript' src="<?= THEME_ASSET_URL ?>assets/js/jquery.customSelect.js"></script>
+
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="<?= THEME_ASSET_URL ?>dist/js/bootstrap.min.js"></script>
 </body>
 
 </html>
